f2629d07805f95658692d60be3a1cf631eea7ad2
tianxin1024 <tianxin4201@163.com>
2022-04-14 15:17:09 +0800

    Expose array to python

--- tests/python/test_cpp.py
+++ tests/python/test_cpp.py
@@ -3,14 +3,24 @@ from txtvm import cpp as txtvm
 def test_basic():
     a = txtvm.Var('a')
     b = txtvm.Var('b')
+    import ipdb
+    ipdb.set_trace()
     c = a + b
 
-
-    assert c == c.lhs
+    assert a == c.lhs
     assert c.dtype == txtvm.int32
     assert txtvm.format_str(c) == '(%s + %s)' % (a.name, b.name)
 
 
+def test_array():
+    a = txtvm.Var('a')
+    x = txtvm.function._symbol([1,2,a])
+    print(type(x))
+    print(len(x))
+    print(x[4])
+
+
 if __name__ == "__main__":
     test_basic()
+    test_array()
     
