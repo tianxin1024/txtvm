de0125a1cf5a8c1e75b03df10529ae614a1d642f
tianxin1024 <tianxin4201@163.com>
2024-01-19 21:57:12 +0800

    add test_domain

--- tests/python/test_domain.py
+++ tests/python/test_domain.py
@@ -11,14 +11,20 @@ def test_range_infer():
 def test_tensor_dom_infer():
     A = txtvm.Tensor(2, name='A')
     B = txtvm.Tensor(2, name='B')
-    T = txtvm.Tensor(3, lambda i, j, k : A(i, k) * B(j, k),
-                     shape=(A.shape[0], B.shape[0], A.shape[1]))
+    # T = txtvm.Tensor(3, lambda i, j, k : A(i, k) * B(j, k),
+    #                  shape=(A.shape[0], B.shape[0], A.shape[1]))
     rd = txtvm.RDom(txtvm.Range(A.shape[1]))
-    C = txtvm.Tensor(2, lambda i, j : txtvm.reduce_sum(T(i, j, rd.index[0]), rdom=rd),
+    # C = txtvm.Tensor(2, lambda i, j : txtvm.reduce_sum(T(i, j, rd.index[0]), rdom=rd),
+    #                  shape=(A.shape[0], B.shape[0]))
+    T = txtvm.Tensor(2, lambda i, j:
+                     txtvm.reduce_sum(A(i, rd.index[0]) * B(j, rd.index[0]), rdom=rd),
+                     shape=(A.shape[0], B.shape[0]))
+    C = txtvm.Tensor(2, lambda i, j: T(i, j),
                      shape=(A.shape[0], B.shape[0]))
 
     cdom = [txtvm.Range(0, 10), txtvm.Range(1, 11)]
     tdom = C.infer_input_domains(cdom, inputs=[T])[T]
+    assert T.is_rtensor
     assert str(tdom[0]) == "(0, 10)"
 
 
