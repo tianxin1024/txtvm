1feed85bef43351c7ceb04f51358aeed6a6e887b
tianxin1024 <tianxin4201@163.com>
2024-01-23 23:16:26 +0800

    add buffer and schedule

---
+++ tests/python/test_buffer.py
@@ -0,0 +1,13 @@
+import txtvm
+
+def test_buffer():
+    buf = txtvm.Buffer(txtvm.Scope.Thread)
+    shape = [32, 16]
+    domain = [txtvm.Range(v) for v in shape]
+    buf.reshape(domain)
+    x = txtvm.Var('x')
+    y = txtvm.Var('y')
+    assert txtvm.format_str(buf(y, x)) == '%s[(%s + (%s * %s))]' % (buf.name, x.name, y.name, shape[1])
+
+if __name__ == "__main__":
+    test_buffer()
